6d128f4134db5b3d23ed949c109958642d50f533
robbpage <buckrussell@gmail.com>
2015-03-12 17:37:39 -0400

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!-- javascript/jquery stuff -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
